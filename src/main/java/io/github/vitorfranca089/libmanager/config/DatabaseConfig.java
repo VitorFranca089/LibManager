fa7805d7eaadfeffa12f032b001d92f0e7894088
Vitor Franca <vitorfranca089@gmail.com>
2024-09-05 15:19:44 -0300

    feat: configure db connection and flyway migrations

---
+++ src/main/java/io/github/vitorfranca089/libmanager/config/DatabaseConfig.java
@@ -0,0 +1,34 @@
+package io.github.vitorfranca089.libmanager.config;
+
+import org.flywaydb.core.Flyway;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+
+    private final static String DB_URL = System.getenv("DB_URL");
+    private final static String DB_USERNAME = System.getenv("DB_USERNAME");
+    private final static String DB_PASSWORD = System.getenv("DB_PASSWORD");
+
+    private static Connection conn;
+
+    public static Connection getConnection(){
+        try {
+            if(conn == null) conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            else return conn;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void migrateDatabase() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(DB_URL, DB_USERNAME, DB_PASSWORD)
+                .load();
+        flyway.migrate();
+    }
+
+}
